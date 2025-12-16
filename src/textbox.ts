4e1d21adb85b2388dc49ac3491bb4e21ccce676b
Davis-Rippon <drip0002@student.monash.edu>
2025-12-17 00:05:31 +1100

    building textbox class, added constants

--- src/textbox.ts
+++ src/textbox.ts
@@ -4,23 +4,37 @@ import { Constants, getObject, parseQuantifier } from "./util";
 export class TextBox {
 	state: State;
 	object: HTMLElement;
-	cursor_idx: number;
+	current_focus: HTMLElement | null;
+	cursor_row_idx: number;
+	cursor_col_idx: number;
 
 	constructor(textbox: HTMLElement) {
-		this.cursor_idx = 0;
-		this.state = [
-			{name: "Name", quantifierA: Constants.EMPTY_QUANTIFIER, quantifierB: Constants.EMPTY_QUANTIFIER}
-		];
 		this.object = textbox;
 
+		this.cursor_row_idx = 0;
+		this.cursor_col_idx = 2; // Starting at 2 as to skip header entry, since it cannot be edited
+
+		this.state = [
+			Constants.HEADER_ENTRY
+		];
+
+		// Render HEADER_ENTRY
 		this.renderEntry();
+
+		// Make first input box
+		this.newEntry();
+
+		this.advanceCursor();
+
 	}
 
+	/*
+	 * Render changes to a specifc entry 
+	 */
 	renderEntry(entry_idx?: number) {
 		// If no entry_idx specified then render the most recent entry
 		if (!entry_idx) {
-			entry_idx = this.cursor_idx;
-			this.cursor_idx++;
+			entry_idx = this.cursor_row_idx;
 		}
 
 		const entry_html = getObject(this.object, `entry-${entry_idx}`);
@@ -36,11 +50,39 @@ export class TextBox {
 		const {name, quantifierA, quantifierB} = entry_obj;
 
 		entry_html.innerHTML = `
-		<div>${name}</div>
-		<div>${parseQuantifier(quantifierA)}</div>
-		<div>${parseQuantifier(quantifierB)}</div>
+		<div id="c0">${name}</div>
+		<div id="c1">${parseQuantifier(quantifierA)}</div>
+		<div id="c2">${parseQuantifier(quantifierB)}</div>
 		`
 		this.object.appendChild(entry_html);
+	}
+
+	/*
+	* Push a blank entry onto the textbox, internally and on the DOM
+	* Doesnt increment row_idx
+	* Can currently only append entry to end of list
+	*/
+	newEntry() {
+		this.state.push(Constants.NEW_ENTRY);
+		const entry_html = getObject(this.object, `entry-${this.cursor_row_idx + 1}`);
+		entry_html.setAttribute("class", "entry");
+		entry_html.innerHTML = `
+		<div id="c0"></div>
+		<div id="c1"></div>
+		<div id="c2"></div>
+		`
+	}
+
+	advanceCursor() {
+		// Remove "contenteditable" from current focus
+		this.current_focus?.removeAttribute("contenteditable");
+
+		if ((this.cursor_col_idx = (this.cursor_col_idx + 1) % 3) === 0) this.cursor_row_idx++;
+		console.log(this.cursor_col_idx);
+		const entry_html = getObject(this.object, `entry-${this.cursor_row_idx}`);
+		this.current_focus = getObject(entry_html, `c${this.cursor_col_idx}`);
+		this.current_focus.setAttribute("contenteditable", "true");
+		this.current_focus.focus();
 	}
 
 }
--- src/util.ts
+++ src/util.ts
@@ -1,14 +1,31 @@
 import type { Quantifier } from "./types";
 
+export const EMPTY_QUANTIFIER: Quantifier = { value: 0, unit: null };
+
+export const HEADER_ENTRY = {
+  name: "<p style=\"text-decoration: underline;\">Exercise</p>",
+  quantifierA: EMPTY_QUANTIFIER,
+  quantifierB: EMPTY_QUANTIFIER
+};
+
+export const NEW_ENTRY = {
+	name: "",
+	quantifierA: EMPTY_QUANTIFIER,
+	quantifierB: EMPTY_QUANTIFIER
+};
+
 export const Constants = {
-	EMPTY_QUANTIFIER: {value: 0, unit: null} as Quantifier
-}
+  EMPTY_QUANTIFIER,
+  NEW_ENTRY,
+  HEADER_ENTRY
+};
 
 export const getObject = (parent: HTMLElement, id: string): HTMLElement => {
 	let obj = parent.querySelector<HTMLElement>(`#${id}`)
 
 	if (!obj) {
 		obj = document.createElement("div");
+		obj.setAttribute("id", id);
 		parent.appendChild(obj);
 	}
 
