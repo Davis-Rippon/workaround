cc7c6ec18358b9dc6e33127e58f16b26531af0a1
Davis-Rippon <drip0002@student.monash.edu>
2025-12-24 22:56:41 +1100

    add advance and retreat cursor

--- src/textbox.ts
+++ src/textbox.ts
@@ -1,5 +1,5 @@
 import type { State } from "./types";
-import { Constants, getObject, parseQuantifier } from "./util";
+import { Constants, getObject, NEW_ENTRY, parseQuantifier } from "./util";
 
 export class TextBox {
 	state: State;
@@ -12,20 +12,16 @@ export class TextBox {
 		this.object = textbox;
 
 		this.cursor_row_idx = 0;
-		this.cursor_col_idx = 2; // Starting at 2 as to skip header entry, since it cannot be edited
+		this.cursor_col_idx = -1; // Dumb but unimportant
 
 		this.state = [
-			Constants.HEADER_ENTRY
+			NEW_ENTRY
 		];
 
+		this.current_focus = null;
+
 		// Render HEADER_ENTRY
-		this.renderEntry();
-
-		// Make first input box
-		this.newEntry();
-
-		this.advanceCursor();
-
+		// this.buildHeader();
 	}
 
 	/*
@@ -64,7 +60,7 @@ export class TextBox {
 	*/
 	newEntry() {
 		this.state.push(Constants.NEW_ENTRY);
-		const entry_html = getObject(this.object, `entry-${this.cursor_row_idx + 1}`);
+		const entry_html = getObject(this.object, `entry-${this.cursor_row_idx}`);
 		entry_html.setAttribute("class", "entry");
 		entry_html.innerHTML = `
 		<div id="c0"></div>
@@ -73,13 +69,51 @@ export class TextBox {
 		`
 	}
 
-	advanceCursor() {
-		// Remove "contenteditable" from current focus
-		this.current_focus?.removeAttribute("contenteditable");
+	advanceCursor = () => {
+		if (this.current_focus) 
+			{
+				this.current_focus.removeAttribute("contenteditable");
+			}
 
-		if ((this.cursor_col_idx = (this.cursor_col_idx + 1) % 3) === 0) this.cursor_row_idx++;
-		console.log(this.cursor_col_idx);
+		if ((this.cursor_col_idx = (this.cursor_col_idx + 1) % 3) === 0) 
+			{
+				console.log("End of row - creating new entry");
+				this.cursor_row_idx++;
+				this.newEntry();
+			}
+
 		const entry_html = getObject(this.object, `entry-${this.cursor_row_idx}`);
+
+		console.log(entry_html);
+
+		this.current_focus = getObject(entry_html, `c${this.cursor_col_idx}`);
+		this.current_focus.setAttribute("contenteditable", "true");
+		this.current_focus.focus();
+	}
+
+
+	retreatCursor = () => {
+		if (this.current_focus) 
+			{
+				this.current_focus.removeAttribute("contenteditable");
+			}
+
+		if ((this.cursor_col_idx = (this.cursor_col_idx - 1)) < 0) 
+			{
+				if (this.cursor_row_idx === 0)
+					{
+						console.log("Cannot retreat cursor");
+						return;
+					}
+				console.log("End of row - creating new entry");
+				this.cursor_row_idx--;
+				this.newEntry();
+			}
+
+		const entry_html = getObject(this.object, `entry-${this.cursor_row_idx}`);
+
+		console.log(entry_html);
+
 		this.current_focus = getObject(entry_html, `c${this.cursor_col_idx}`);
 		this.current_focus.setAttribute("contenteditable", "true");
 		this.current_focus.focus();
