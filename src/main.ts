1075c43f4fd2a626705170436c2abca2b13c40ac
Davis-Rippon <drip0002@student.monash.edu>
2025-12-26 22:38:31 +1100

    fix html

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,21 @@ import './style.css'
 import { TextBox } from './textbox';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <h1>Workaround</h1>
+<div>
+	<h1>Workaround</h1>
 	<div id="textbox" class="textbox"></div>
-	<button id="tab">→</button>
-  </div>
+		<div class="navigation">
+			<button id="tab-l">←</button>
+			<button id="tab-r">→</button>
+		</div>
+	</div>
+</div>
 `
 
 const tb = new TextBox(document.getElementById("textbox")!)
 
 // Bind tab button and key to advance cursor
-document.getElementById("tab")?.addEventListener('click', tb.advanceCursor);
+document.getElementById("tab-r")?.addEventListener('click', tb.advanceCursor);
 document.addEventListener('keydown', (e) => {
 	if (e.key === "Tab" && e.shiftKey) {
 		console.log("SHIFTTBA");
