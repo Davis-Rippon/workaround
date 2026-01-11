0d17cad25a3c38083a4b9a171c76c07141eaaf73
Davis-Rippon <drip0002@student.monash.edu>
2026-01-12 09:01:55 +1100

    Add completion engine class

---
+++ src/completion_engine.ts
@@ -0,0 +1,24 @@
+/*
+    * Autocomplete engine for exercises
+*/
+
+import { mongeElkan } from "./util";
+
+export class CompletionEngine {
+    tokenisedWords: { tokens: string[]; original: string }[] = [];
+
+    constructor(strs: string[]) {
+        for (const str of strs) {
+            this.tokenisedWords.push({ tokens: str.split(' '), original: str });
+        }
+    }
+    
+    public query(s: String[]): String[] {
+        const out = this.tokenisedWords
+        .slice()
+        .sort((a, b) => { return mongeElkan(s, a.tokens) - mongeElkan(s, b.tokens)})
+        .map(x => x.original);
+
+        return out;
+    }
+}
